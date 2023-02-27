7aeb551da2ed6be5ac56d22c9bfc880ee3d969fa
Olga Vaikul <olenka1201@list.ru>
2023-02-27 11:08:28 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -39,6 +39,25 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+
+    let oneWord = expr.split('**********')
+    
+    let oneLetter = [];
+    for (let i = 0; i < oneWord.length; i++ ){
+        oneLetter.push(oneWord[i].match(/.{10}/g))
+    }
+    let morseDecoder = [];
+for (let j = 0; j < oneLetter.length; j++) {
+    let toMourse = []
+
+    for (let k = 0; k <oneLetter[j].length; k++) {
+        toMourse.push(oneLetter[j][k].replace(/10/g,'.').replace(/11/g,'-').replace(/00/g,''))
+    }
+morseDecoder.push((toMourse.map(i => MORSE_TABLE[i])).join(''))
+
+}
+return morseDecoder.join(' ')
+
 }
 
 module.exports = {
